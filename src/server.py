29f20afb23a5c25a8100e745b4951ccc93ac1e04
EvenSol <esolbraa@gmail.com>
2020-12-20 14:08:31 +0100

    start

---
+++ src/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, jsonify
+
+server = Flask(__name__)
+
+@server.route("/")
+def hello():
+   return "Hello from NeqSim API!"
+
+if __name__ == "__main__":
+   server.run(host='0.0.0.0', debug=True)
